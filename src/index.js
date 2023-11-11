f08f352f9808a45b3552e02e0c77063ae38ebc7e
nhattinh2601 <nhattinh2601@gmail.com>
2023-11-12 01:14:22 +0700

    thay doi info user + change password

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import Login from './component/Login';
 import Register from './component/Register.js';
 import ChiTietGiangVien from './component/ChiTietGiangVien'
 import ChiTietKhoaHoc from './component/ChiTietKhoaHoc'
+import ChinhSuaInfo from './component/user/ChinhSuaInfo'
+import DoiMatKhau from './component/user/DoiMatKhau'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -19,6 +21,8 @@ root.render(
         <Route path="/register" element={<Register />} />   
         <Route path="/chitietgiangvien" element={<ChiTietGiangVien />} /> 
         <Route path="/chitietkhoahoc" element={<ChiTietKhoaHoc />} /> 
+        <Route path="/user/chinhsuainfo" element={<ChinhSuaInfo />} /> 
+        <Route path="/user/doimatkhau" element={<DoiMatKhau />} /> 
       </Routes>
     </Router>
   </React.StrictMode>
