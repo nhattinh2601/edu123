0a0e8188337a0e76ed30ef0a4ec6cfc6377db840
TMHieu02 <hieultkrm1@gmail.com>
2023-11-25 05:02:45 +0700

    Gửi mã otp xác nhận để đổi mật khẩu khi quên

--- src/component/User/MyProfile/ForgetPassword.js
+++ src/component/User/MyProfile/ForgetPassword.js
@@ -1,151 +1,202 @@
 import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import axiosClient from "../../../api/axiosClient";
 import { Link, useNavigate } from "react-router-dom";
-import { forgetPassword } from "../../../thunks/authThunk";
 
-function ForgetPassWord() {
-  const dispatch = useDispatch();
+function ForgetPassword() {
+  const [email, setEmail] = useState("");
+  const [confirmationCode, setConfirmationCode] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [isEmailSubmitted, setIsEmailSubmitted] = useState(false);
+  const [resetPasswordStatus, setResetPasswordStatus] = useState("idle");
+  const [resetPasswordError, setResetPasswordError] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
   const navigate = useNavigate();
-  const resetPasswordStatus = useSelector(
-    (state) => state.auth.resetPasswordStatus
-  );
-  const resetPasswordError = useSelector(
-    (state) => state.auth.resetPasswordError
-  );
-
-  const [formData, setFormData] = useState({
-    email: "",
-    newPassword: "",
-    rePassword: "",
-  });
 
   const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+    setEmail(e.target.value);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if (!formData.email.includes("@")) {
-      alert("Email không hợp lệ. Vui lòng nhập đúng định dạng email.");
+    if (!isEmailSubmitted) {
+      sendOtp();
+    } else {
+      confirmResetPassword();
+    }
+  };
+
+  const sendOtp = () => {
+    setResetPasswordStatus("loading");
+
+    axiosClient
+      .post(`/auth/forgetpassword/sendotp/${email}`)
+      .then((response) => {
+        setIsEmailSubmitted(true);
+        setResetPasswordStatus("success");
+        setSuccessMessage("Mã xác nhận đã được gửi thành công.");
+        console.log(response.data);
+      })
+      .catch((error) => {
+        setResetPasswordStatus("error");
+        setResetPasswordError(error.response.data);
+        console.error(error.response.data);
+      });
+  };
+
+  const confirmResetPassword = () => {
+    if (newPassword.length < 6) {
+      setResetPasswordStatus("error");
+      setResetPasswordError("Mật khẩu phải có ít nhất 6 ký tự.");
       return;
     }
 
-    if (formData.newPassword.length < 6) {
-      alert("Mật khẩu phải có ít nhất 6 kí tự.");
+    if (newPassword !== confirmPassword) {
+      setResetPasswordStatus("error");
+      setResetPasswordError("Mật khẩu xác nhận không khớp.");
       return;
     }
 
-    if (formData.newPassword !== formData.rePassword) {
-      alert("Xác nhận mật khẩu không khớp. Vui lòng nhập lại.");
-      return;
-    }
+    setResetPasswordStatus("loading");
 
-    dispatch(forgetPassword(formData)).then((success) => {
-      if (success) {
-        localStorage.removeItem("rememberedEmail");
-        localStorage.removeItem("rememberedPassword");
-        navigate("/login");
-      }
-    });
+    axiosClient
+      .post(`/auth/confirm-reset-password/${email}`, {
+        confirmationCode,
+        newPassword,
+      })
+      .then((response) => {
+        setResetPasswordStatus("success");
+        setSuccessMessage(
+          "Đổi mật khẩu thành công. Đang chuyển hướng đến trang đăng nhập..."
+        );
+        setTimeout(() => {
+          setSuccessMessage("");
+          navigate("/login");
+        }, 2000);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        setResetPasswordStatus("error");
+        setResetPasswordError(error.response.data);
+        console.error(error.response.data);
+      });
   };
 
   return (
     <div className="container">
-      <div className="row">
-        <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
+      <div className="row justify-content-center align-items-center min-vh-100">
+        <div className="col-sm-9 col-md-7 col-lg-5">
           <div className="card border-0 shadow rounded-3 my-5">
             <div className="card-body p-4 p-sm-5">
-              <h5 className="card-title text-center mb-5 fw-light fs-10 fw-bold text-decoration-underline">
-                Đổi mật khẩu
-              </h5>
-              <div>
-                <label className="form-label" htmlFor="fullName">
-                  <span style={{ color: "red" }}>* Bắt buộc điền</span>
-                </label>
-              </div>
+              <h2 className="text-center mb-5">
+                {isEmailSubmitted
+                  ? "Xác nhận mã và Đổi mật khẩu"
+                  : "Quên mật khẩu"}
+              </h2>
               <form onSubmit={handleSubmit}>
-                <div className="form-outline mb-4">
-                  <div>
-                    <label className="form-label" htmlFor="fullName">
-                      <span style={{ color: "red" }}>*</span>
+                {!isEmailSubmitted && (
+                  <div className="mb-4">
+                    <label htmlFor="email" className="form-label fw-bold">
+                      Email
                     </label>
+                    <input
+                      type="email"
+                      className="form-control"
+                      name="email"
+                      id="email"
+                      value={email}
+                      onChange={handleChange}
+                      required
+                    />
                   </div>
-                  <label className="form-label fw-bold" htmlFor="email">
-                    Email
-                  </label>
-                  <input
-                    type="email"
-                    className="form-control"
-                    name="email"
-                    id="email"
-                    value={formData.email}
-                    onChange={handleChange}
-                    required
-                  />
-                </div>
+                )}
 
-                <div className="form-outline mb-4">
+                {isEmailSubmitted && (
                   <div>
-                    <label className="form-label" htmlFor="fullName">
-                      <span style={{ color: "red" }}>*</span>
-                    </label>
-                  </div>
-                  <label className="form-label fw-bold" htmlFor="newPassword">
-                    Mật khẩu mới
-                  </label>
-                  <input
-                    type="password"
-                    className="form-control"
-                    name="newPassword"
-                    id="newPassword"
-                    value={formData.newPassword}
-                    onChange={handleChange}
-                    required
-                  />
-                </div>
+                    <div className="mb-4">
+                      <label
+                        htmlFor="confirmationCode"
+                        className="form-label fw-bold"
+                      >
+                        Mã xác nhận
+                      </label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        name="confirmationCode"
+                        id="confirmationCode"
+                        value={confirmationCode}
+                        onChange={(e) => setConfirmationCode(e.target.value)}
+                        required
+                      />
+                    </div>
 
-                <div className="form-outline mb-4">
-                  <div>
-                    <label className="form-label" htmlFor="fullName">
-                      <span style={{ color: "red" }}>*</span>
-                    </label>
+                    <div className="mb-4">
+                      <label
+                        htmlFor="newPassword"
+                        className="form-label fw-bold"
+                      >
+                        Mật khẩu mới
+                      </label>
+                      <input
+                        type="password"
+                        className="form-control"
+                        name="newPassword"
+                        id="newPassword"
+                        value={newPassword}
+                        onChange={(e) => setNewPassword(e.target.value)}
+                        required
+                      />
+                    </div>
+
+                    <div className="mb-4">
+                      <label
+                        htmlFor="confirmPassword"
+                        className="form-label fw-bold"
+                      >
+                        Nhập lại mật khẩu mới
+                      </label>
+                      <input
+                        type="password"
+                        className="form-control"
+                        name="confirmPassword"
+                        id="confirmPassword"
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                        required
+                      />
+                    </div>
                   </div>
-                  <label className="form-label fw-bold" htmlFor="rePassword">
-                    Nhập lại mật khẩu mới
-                  </label>
-                  <input
-                    type="password"
-                    className="form-control"
-                    name="rePassword"
-                    id="rePassword"
-                    value={formData.rePassword}
-                    onChange={handleChange}
-                    required
-                  />
-                </div>
+                )}
 
                 {resetPasswordError && (
                   <div className="alert alert-danger" role="alert">
                     {resetPasswordError}
                   </div>
                 )}
-                <button
-                  type="submit"
-                  className="btn btn-success btn-block mb-4 w-50" 
-                  disabled={resetPasswordStatus === "loading"}
-                >
-                  {resetPasswordStatus === "loading"
-                    ? "Đang xử lý..."
-                    : "Lấy lại mật khẩu"}
-                </button>
 
-                <Link
-                  to="/login"
-                  className="btn btn-primary btn-block mb-4 w-50"
-                >
-                  Đăng nhập
-                </Link>
+                {successMessage && (
+                  <div className="alert alert-success" role="alert">
+                    {successMessage}
+                  </div>
+                )}
+
+                <div className="text-center"> {/* Centered buttons */}
+                  <button
+                    type="submit"
+                    className="btn btn-primary btn-block mb-4"
+                    disabled={resetPasswordStatus === "loading"}
+                  >
+                    {resetPasswordStatus === "loading"
+                      ? "Đang xử lý..."
+                      : isEmailSubmitted
+                      ? "Xác nhận và Đổi mật khẩu"
+                      : "Gửi mã xác nhận"}
+                  </button>
+
+                  
+                </div>
               </form>
             </div>
           </div>
@@ -155,4 +206,4 @@ function ForgetPassWord() {
   );
 }
 
-export default ForgetPassWord;
+export default ForgetPassword;
